3ae5262d7bf4804234331e7e5f8d98f193846270
wong2 <wonderfuly@gmail.com>
2013-08-06 17:59:50 +0800

    add local config

--- config.py
+++ config.py
@@ -1,9 +1,14 @@
 #-*-coding:utf-8-*-
 
-APP_KEY = '092a0eeb1abc4e45beb42d0f62cd62bc'
-APP_SECRET = 'dccc25f668bf4a8bb143ef0dda327cea'
-REDIRECT_URL = 'http://localhost:5000/callback'
+APP_KEY = ''
+APP_SECRET = ''
+REDIRECT_URL = ''
 
-SERVER_HOST = "localhost"
-SERVER_IPv6 = False
+SERVER_HOST = ''
+SERVER_IPv6 = True
 SERVER_PORT = 9991
+
+try:
+    from local_config import *
+except:
+    pass
